c61439a681ea4ed6c76d3228b6aaf0453ca7721c
Matt Miller <thisismattmiller@gmail.com>
2016-04-12 10:35:45 -0400

    first

---
+++ index.js
@@ -0,0 +1,10 @@
+var ingestElastic = require("./lib/ingest_elastic")
+var compare = require("./lib/compare")
+var dbLoad = require("./lib/db_load")
+
+
+
+//ingestElastic.ingest()
+compare.compare()
+
+//dbLoad()
